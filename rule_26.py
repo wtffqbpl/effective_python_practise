f1a98fb9e5bab43e8485fb3dfcc123c8a391e99d
Yuanjun Ren <renyj@buaa.edu.cn>
2018-01-10 00:12:29 +0800

    rule 26, 27

---
+++ rule_26.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python
+# coding:utf-8
+# Author: Yuanjun Ren
+
+import json
+
+
+class ToDictMixin(object):
+    def to_dict(self):
+        return self._traverse_dict(self.__dict__)
+
+    def _traverse_dict(self, instance_dict):
+        output = dict()
+        for key, value in instance_dict.items():
+            output[key] = self._traverse(key, value)
+        return output
+
+    def _traverse(self, key, value):
+        if isinstance(value, ToDictMixin):
+            return value.to_dict()
+        elif isinstance(value, dict):
+            return self._traverse_dict(value)
+        elif isinstance(value, list):
+            return [self._traverse(key, i) for i in value]
+        elif hasattr(value, '__dict__'):
+            return self._traverse_dict(value.__dict__)
+        else:
+            return value
+
+
+class BinaryTree(ToDictMixin):
+    def __init__(self, value, left=None, right=None):
+        self.value = value
+        self.left = left
+        self.right = right
+
+
+class BinaryTreeWithParent(BinaryTree):
+    def __init__(self, value, left=None, right=None, parent=None):
+        super().__init__(value, left=left, right=right)
+        self.parent = parent
+
+    def _traverse(self, key, value):
+        if (isinstance(value, BinaryTreeWithParent) and
+                key == "parent"):
+            return value.value # parent cycles
+        else:
+            return super()._traverse(key, value)
+
+
+class NamedSubTree(ToDictMixin):
+    def __init__(self, name, tree_with_parent):
+        self.name = name
+        self.tree_with_parent = tree_with_parent
+
+
+class JsonMixin(object):
+    @classmethod
+    def from_json(cls, data):
+        kwargs = json.loads(data)
+        return cls(**kwargs)
+
+    def to_json(self):
+        return json.dumps(self.to_dict())
+
+
+class DatacenterRack(ToDictMixin, JsonMixin):
+    def __init__(self, switch=None, machines=None):
+        self.switch = switch
+        self.machines = [Machine(**kwargs) for kwargs in machines]
+    pass
+
+
+class Switch(ToDictMixin, JsonMixin):
+    def __init__(self, switch=None):
+        self.switch = switch
+
+
+class Machine(ToDictMixin, JsonMixin):
+    def __init__(self, machine=None):
+        self.machine = machine
+
+
+if __name__ == "__main__":
+
+    serialized = """
+    {
+        "switch": {"ports": 5, "speed": 1e9},
+        "machines": [
+            {"cores": 8, "ram": 32e9, "disk": 5e12},
+            {"cores": 4, "ram": 16e9, "disk": 1e12},
+            {"cores": 2, "ram": 4e9, "disk": 500e9},
+        ]
+    }
+    """
+    deserialized = DatacenterRack.from_json(serialized)
+    roundtrip = deserialized.to_json()
+    assert json.loads(serialized) == json.loads(roundtrip)
---
+++ rule_27.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# coding:utf-8
+# Author: Yuanjun Ren
+
+
+class MyOtherObject(object):
+    def __init__(self):
+        self.__private_field = 71
+
+
+if __name__ == "__main__":
+    pass
